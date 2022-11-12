89d168e3d860f5be42c01b8b9dfb48fcf6d6b952
Derek Herincx <derek663@gmail.com>
2022-11-12 15:13:47 -0500

    Adding error handling for invalid arguments in fpds atom; TODO -- adjust config file path

--- src/fpds/cli/fpds.py
+++ src/fpds/cli/fpds.py
@@ -1,31 +1,70 @@
+import re
+
 import click
 from click import Argument, Choice, Path, UsageError
 
+from src.fpds.core.exceptions.errors import (
+    InvalidParameter,
+    InvalidParameterInput
+)
+
+# testing purposes
+import json
+
+CONFIG = "/Users/dherincx/projects/git/fpds/src/fpds/core/constants/fields.json"
+with open(CONFIG) as file:
+    cfg = json.load(file)
 
 @click.command()
 @click.argument("params", nargs=-1)
 def parse(params):
     """
     Parsing command to retrieve results from the FPDS ATOM feed
+
     \b
     Usage:
         $ fpds parse [PARAMS]
+
     \b
     Positional Argument(s):
         PARAMS  Search criteria parameters for filtering response
 
+        \b
         Reference the Atom Feed Usage documentation at
         https://www.fpds.gov/wiki/index.php/Atom_Feed_Usage to determine
         how to format parameters for the CLI. As an example, if a user
         wants to filter for AWARD contract types, the parameter criteria
-        should look like this: `'CONTRACT_TYPE=”AWARD”'`. All parameter
-        strings should be surrounded by single quotes to protect against
-        errors where the value of the parameter is a string like the
-        example above. A full cli command could look like this:
-        `fpds parse params "LAST_MODE_DATE=[2022/01/01, 2022/05/01]" "AGENCY_CODE=7504"
+        should look like this: 'CONTRACT_TYPE=”AWARD”'. A full CLI command
+        could look like this:
+
+        \b
+            fpds parse params "LAST_MODE_DATE=[2022/01/01, 2022/05/01]" "AGENCY_CODE=7504"
     """
     params = [param.split("=") for param in params[1:]]
-    params_kwargs = dict(params)
-    
+    field_names = [field.get("name") for field in cfg]
 
-    click.echo(params_kwargs)
+    for param_tuple in params:
+        # checks that a param is a valid FPDS param
+        param_name, param_input = param_tuple
+        if param_name not in field_names:
+            raise InvalidParameter(param=param_name)
+
+        get_field_dict = lambda field: field.get("name") == param_name
+        field_regex = list(filter(get_field_dict, cfg))[0].get("regex")
+
+        # does the param input match expected regex pattern?
+        raw_pattern = fr"{field_regex}".replace("\\\\", "\\")
+        LITERAL_PATTERN = re.compile(raw_pattern)
+        match = LITERAL_PATTERN.match(param_input)
+        if not match:
+            raise InvalidParameterInput(input=param_input, regex=raw_pattern)
+
+        # enclose param value in quotes, if required by the Atom feed
+        quotes = list(filter(get_field_dict, cfg))[0].get("quotes")
+        if quotes:
+            param_tuple[1] = f'"{param_input}"'
+    params_kwargs = dict(params)
+
+    click.echo(f"Params to be used for FPDS search: {params_kwargs}")
+
+    return params_kwargs
---
+++ src/fpds/core/exceptions/errors.py
@@ -0,0 +1,14 @@
+class InvalidParameter(Exception):
+    """Identifies invalid parameters for the `fpds` CLI"""
+    def __init__(self, param):
+        self.param = param
+        self.message = f"`{self.param}` is not a valid parameter"
+        super().__init__(self.message)
+
+class InvalidParameterInput(Exception):
+    """Identifies invalid parameters for the `fpds` CLI"""
+    def __init__(self, input, regex):
+        self.input = input
+        self.regex = regex
+        self.message = f"{self.input} does not match regex: {self.regex}"
+        super().__init__(self.message)
