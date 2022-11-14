8b982601376dae907ba61aa0eb1752982b45c34e
Derek Herincx <derek663@gmail.com>
2022-11-14 10:17:10 -0500

    Update error docstring

--- src/fpds/core/exceptions/errors.py
+++ src/fpds/core/exceptions/errors.py
@@ -4,15 +4,16 @@ Custom exceptions for validating ATOM feed search parameters
 author: derek663@gmail.com
 last_updated: 11/13/2022
 """
+
 class InvalidParameter(Exception):
-    """Identifies invalid parameters for the `fpds` CLI"""
+    """Raised if invalid parameter used with `fpds` CLI"""
     def __init__(self, param):
         self.param = param
         self.message = f"`{self.param}` is not a valid parameter"
         super().__init__(self.message)
 
 class InvalidParameterInput(Exception):
-    """Identifies invalid parameters for the `fpds` CLI"""
+    """Raised if input doesnt match specified regex"""
     def __init__(self, input, regex):
         self.input = input
         self.regex = regex
