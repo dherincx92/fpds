5b8878e72bced2a764d6fd13e9ebfdc7869cf367
Derek Herincx <derek663@gmail.com>
2022-11-15 17:18:59 -0500

    Adding a config file and using importlib to pull fields.json properly when packing this module

--- src/fpds/cli/fpds.py
+++ src/fpds/cli/fpds.py
@@ -1,20 +1,16 @@
 import json
 import os
 import re
-from pathlib import Path
 
 import click
 
+from src.fpds.config import FIELDS_FILE_CONFIG as FIELDS
 from src.fpds.core.exceptions.errors import (
     InvalidParameter,
     InvalidParameterInput
 )
 from src.fpds.core.parser import fpdsRequest
 
-CONFIG_DIR = os.getcwd()
-CONFIG = os.path.join(CONFIG_DIR, "constants", "fields.json")
-with open(CONFIG) as file:
-    cfg = json.load(file)
 
 @click.command()
 @click.argument("params", nargs=-1)
@@ -42,7 +38,7 @@ def parse(params):
             fpds parse params "LAST_MOD_DATE=[2022/01/01, 2022/05/01]" "AGENCY_CODE=7504"
     """
     params = [param.split("=") for param in params[1:]]
-    field_names = [field.get("name") for field in cfg]
+    field_names = [field.get("name") for field in FIELDS]
 
     if not params:
         raise ValueError("Please provide parameters for FPDS ATOM feed")
@@ -54,7 +50,7 @@ def parse(params):
             raise InvalidParameter(param=param_name)
 
         get_field_dict = lambda field: field.get("name") == param_name
-        field_regex = list(filter(get_field_dict, cfg))[0].get("regex")
+        field_regex = list(filter(get_field_dict, FIELDS))[0].get("regex")
 
         # does the param input match expected regex pattern?
         raw_pattern = fr"{field_regex}".replace("\\\\", "\\")
@@ -64,7 +60,7 @@ def parse(params):
             raise InvalidParameterInput(input=param_input, regex=raw_pattern)
 
         # enclose param value in quotes, if required by the Atom feed
-        quotes = list(filter(get_field_dict, cfg))[0].get("quotes")
+        quotes = list(filter(get_field_dict, FIELDS))[0].get("quotes")
         if quotes:
             param_tuple[1] = f'"{param_input}"'
 
---
+++ src/fpds/config.py
@@ -0,0 +1,15 @@
+"""
+Configurations and constants
+
+author: derek663@gmail.com
+last_updated: 11/15/2022
+"""
+import json
+from importlib.resources import files
+from pathlib import Path
+
+FIELDS_FILE = "fields.json"
+FPDS_FIELDS_FILE = files('src.fpds.core.constants').joinpath(FIELDS_FILE)
+
+with Path(FPDS_FIELDS_FILE).open(encoding="utf-8") as file:
+    FIELDS_FILE_CONFIG = json.load(file)
