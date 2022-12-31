5f54cea25dec0a90d1f1fe967b89f1f5afe969c3
Derek Herincx <derek663@gmail.com>
2022-12-31 03:05:48 -0500

    Reformatting cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,9 +4,6 @@ from unittest import TestCase
 from click.testing import CliRunner
 from fpds.cli import cli
 
-invalid_cli_params = ["parse", "params", "{an-invalid-param}={some-value}"]
-invalid_cli_param_regex = ["parse", "params", "AGENCY_CODE={not-valid}"]
-
 
 class TestFpdsCLI(TestCase):
     def setUp(self):
@@ -17,11 +14,11 @@ class TestFpdsCLI(TestCase):
         assert "Please provide at least one parameter" in result.output
 
     def test_invalid_cli_parameter(self):
-        result = self.runner.invoke(cli, invalid_cli_params)
+        result = self.runner.invoke(cli, ["parse", "{an-invalid-param}={some-value}"])
         assert "is not a valid parameter" in result.output
 
     def test_invalid_cli_parameter_regex_pattern(self):
-        result = self.runner.invoke(cli, invalid_cli_param_regex)
+        result = self.runner.invoke(cli, ["parse", "AGENCY_CODE={not-valid}"])
         assert "does not match regex" in result.output
 
 
