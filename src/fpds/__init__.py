1548dec6e191b414e14e36081ba01cade5595562
Derek Herincx <derek663@gmail.com>
2023-05-03 23:34:29 -0400

    Fix import to get tests passing; again a v1

--- src/fpds/__init__.py
+++ src/fpds/__init__.py
@@ -1,3 +1,7 @@
 from .core.parser import fpdsRequest
+from .core._xml import fpdsXML
 
-__all__ = ["fpdsRequest"]
+__all__ = [
+    "fpdsRequest",
+    "fpdsXML"
+]
--- src/fpds/utilities/decorators.py
+++ src/fpds/utilities/decorators.py
@@ -1,10 +1,15 @@
+"""
+Utility decorators for fpds API
+
+author: derek663@gmail.com
+last_updated: 2023/05/03
+"""
 import json
 from functools import wraps
 
 
 def jsonify(method):
     """Jsonifies data"""
-
     @wraps(method)
     def wrapper(self, *args, **kwargs):
         data = method(self, *args, **kwargs)
